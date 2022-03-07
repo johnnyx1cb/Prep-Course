0bd2b2823f7f780a407bc57692d157200b5336c7
johnnyx1cb <johnnyx1cb@gmail.com>
2022-03-07 18:26:32 -0500

    Homework 1

---
+++ 02-JS-I/homework/homework_copy.js
@@ -0,0 +1,6 @@
+function redondearHaciaArriba(num) {
+  // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
+  // Tu código:
+  return Math.ceil(num); //->0.2 -->
+}
+console.log(redondearHaciaArriba(0.4))
