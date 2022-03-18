bec7b082e9f42540454fd3643d1e642a16c802c5
johnnyx1cb <johnnyx1cb@gmail.com>
2022-03-18 01:11:28 -0500

    xd

--- 02-JS-I/homework/para practicar 1.js
+++ 02-JS-I/homework/para practicar 1.js
@@ -517,3 +517,15 @@ function dePalabrasAFrase(palabras) {
         palabras.join(' ');
         return palabras;
 }
+
+function arrayContiene(array, elemento) {
+    // Comprueba si el elemento existe dentro de "array"
+    // Devuelve "true" si está, o "false" si no está
+    // Tu código:
+    for (var i = 0; i < array.length; i++) {
+        if(array[i] === elemento){
+            return true;
+        }
+    }
+    return false;
+}
