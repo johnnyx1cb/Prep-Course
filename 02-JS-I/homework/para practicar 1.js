79f38d6667c5cf2ee1c41ca407fa766715e733e5
johnnyx1cb <johnnyx1cb@gmail.com>
2022-03-22 10:37:54 -0500

    xd

--- 02-JS-I/homework/para practicar 1.js
+++ 02-JS-I/homework/para practicar 1.js
@@ -686,3 +686,18 @@ function continueStatement(numero) {
      return arr;       
 }
 
+function crearGato(nombre, edad) {
+    // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
+    // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+    // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
+    // Devuelve el objeto
+    // Tu código:
+    var obj = {
+        nombre : nombre,
+        edad : edad,
+        meow : function gato(){
+            return "Meow!"
+        }
+    }
+    return obj
+}
