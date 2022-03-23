48e3c8d3975f4e9b5f607278cd452892aee2c418
johnnyx1cb <johnnyx1cb@gmail.com>
2022-03-22 22:02:42 -0500

    xd

--- 02-JS-I/homework/para practicar 1.js
+++ 02-JS-I/homework/para practicar 1.js
@@ -701,3 +701,105 @@ function crearGato(nombre, edad) {
     }
     return obj
 }
+
+function invocarMetodo(objeto, metodo) {
+    // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
+    // Invoca ese método
+    // Nada necesita ser devuelto ("returned")
+    // Tu código:
+    objeto[metodo]();
+}
+
+function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
+    // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
+    // Multiplica el numeroMisterioso por 5 y devuelve el producto
+    // Tu código:
+    var num = objetomisterioso[numeroMiaterioso];
+    return num * 5;
+}
+
+function eliminarPropiedad(objeto, unaPropiedad) {
+    // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad 
+    // tip: tenes que usar bracket notation
+    // Devuelve el objeto
+    // Tu código:
+    delete objeto[unaPropiedad];
+    return objeto;
+}
+
+function nuevoUsuario(nombre, email, password) {
+    // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
+    // Devuelve el objeto
+    // Tu código:
+    var obj = {
+      nombre: nombre,
+      email: email,
+      password: password,
+    }
+      return obj;
+  }
+
+
+function tieneEmail(usuario) {
+    // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
+    // De lo contratio, devuelve "false"
+    // Tu código:
+    if(usuario.email){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function tienePropiedad(objeto, propiedad) {
+    // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
+    // "propiedad" es un string
+    // De lo contrario, devuelve "false"
+    // Tu código:
+    if(objeto[propiedad]){
+        return true;
+    }
+      return false;
+}
+
+
+function verificarPassword(usuario, password) {
+    // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
+    // Devuelve "true" si coinciden
+    // De lo contrario, devuelve "false"
+    // // Tu código:
+    if(usuario.password === password){
+        return true;
+    }
+        return false;
+}
+
+function actualizarPassword(usuario, nuevaPassword) {
+    // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
+    // Devuelve el objeto
+    // Tu código:
+    usuario.password = nuevaPassword;
+    return usuario;
+}
+
+function agregarAmigo(usuario, nuevoAmigo) {
+    // "usuario" tiene una propiedad llamada "amigos" que es un array
+    // Agrega "nuevoAmigo" al final de ese array
+    // Devuelve el objeto "usuario"
+    // // Tu código:
+    usuario.amigos.push(nuevoAmigo);
+    return usuario;
+}
+
+function pasarUsuarioAPremium(usuarios) {
+    // "usuarios" es un array de objetos "usuario"
+    // Cada objeto "usuario" tiene la propiedad "esPremium"
+    // Define cada propiedad "esPremium" de cada objeto como "true"
+    // Devuelve el array de usuarios
+    // Tu código:
+    // usuarios = [usuario = {esPremium: false}, usuario = {esPremium: false}...]
+    for (var i = 0; i < usuarios.length; i++) {
+        usuarios[i].esPremium = true;
+    }
+    return usuarios;
+}
